96c7cabd51a88f2dcfd88f48836a8afc980e886f
Mr Zoom <mr.zoom.orsk@gmail.com>
2022-05-27 18:33:25 +0500

    Release 1.1!

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
     var socket = io('http://localhost:5000/');
 
     socket.on('connect', function () {
-
         socket.emit('add_sid', {data: window.location.href});
+    })
+    socket.on('user_update', function (data) {
+        console.log("ZASHEL");
+        if(window.location.href.slice(window.location.href.length - 1) == data.data){
+            console.log(data.data);
+            document.getElementById("last_seen").textContent = data.last_seen;
+        }
     })
 
     window.onbeforeunload = function () {
