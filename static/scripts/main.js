fc9c08e0266f75dc29e25a0da4b4d5d888674100
Mr Zoom <mr.zoom.orsk@gmail.com>
2022-06-06 20:14:00 +0500

    Finish develop lazy loading and add gif loading

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,15 +1,11 @@
-$(".messenger").scroll(function () {
+const myDiv = document.querySelector('.messenger')
+myDiv.addEventListener('scroll', () => {
+  if (myDiv.offsetHeight + myDiv.scrollTop >= myDiv.scrollHeight) {
+    localStorage.setItem('dialog', parseInt(localStorage.getItem('dialog')) + 5)
     console.log(localStorage.getItem('dialog'))
-    s_top = $(".messenger").scrollTop();
-    yes = $("#ul" +  localStorage.getItem('dialog')).offset().top;
-
-    if(s_top > yes - 800){
-         localStorage.setItem('dialog',  localStorage.getItem('dialog') + 5)
-        messenger()
-        console.log("Yes");
-    }
-});
-
+    messenger()
+  }
+})
 
 
 function formatData (data) {
@@ -61,12 +57,11 @@ $(document).ready(function() {
     socket.on('connect', function () {
         localStorage.setItem('dialog', 5)
         messenger()
+        messenger_set_select()
         socket.emit('add_sid', {data: id_real});
     })
     socket.on('user_update', function (data) {
-        console.log("ZASHEL");
         if(window.location.href.slice(window.location.href.length - 1) == data.data){
-            console.log(data.data);
             document.getElementById("last_seen").textContent = data.last_seen;
         }
     })
@@ -82,35 +77,51 @@ $(document).ready(function() {
 
     //вешаем на него событие
         a.onclick = function() {
-            console.log("!!!!!!!!!")
             return false;
         }
     }
 
 })
+
+function messenger_set_select () {
+    this_select = document.getElementById('choose_dialog')
+    var length = this_select.options.length;
+    for (i = length-1; i >= 0; i--) {
+        this_select.options[i] = null;
+    }
+    var opt_first = document.createElement('option');
+    opt_first.value = "";
+    opt_first.innerHTML = "";
+    this_select.appendChild(opt_first);
+    fetch('http://localhost:5000/api/v1/dialogs')
+    .then((response) => {
+        return response.json();
+    })
+    .then((myjson) => {
+            dialogs = myjson.dialogs;
+            for (let i = 0; i < dialogs.length; i++) {
+                var opt = document.createElement('option');
+                opt.value = dialogs[i].id;
+                opt.innerHTML = dialogs[i].title;
+                this_select.appendChild(opt);
+            }
+    })
+}
+
 function messenger () {
-        this_select = document.getElementById('choose_dialog')
+        var ul = document.getElementById("ul_messenger_id");
+        ul.innerHTML += `<li><div id="mmm">
+<!--        https://i.gifer.com/VAyR.gif-->
+        <img src="../static/gif/loading.gif" alt="я джифка" class="gif_loading">
+    </div></li>`
         fetch('http://localhost:5000/api/v1/dialogs')
         .then((response) => {
             return response.json();
         })
         .then((myjson) => {
             dialogs = myjson.dialogs;
-            var length = this_select.options.length;
-            for (i = length-1; i >= 0; i--) {
-                this_select.options[i] = null;
-            }
-            var opt_first = document.createElement('option');
-                opt_first.value = "";
-                opt_first.innerHTML = "";
-                this_select.appendChild(opt_first);
-            for (let i = 0; i < localStorage.getItem('dialog'); i++){
-                console.log(i)
-                var opt = document.createElement('option');
-                opt.value = dialogs[i].id;
-                console.log(dialogs[i].id)
-                opt.innerHTML = dialogs[i].title;
-                this_select.appendChild(opt);
+            if(parseInt(localStorage.getItem('dialog')) < dialogs.length) {
+                for (let i = localStorage.getItem('dialog') - 5; i < localStorage.getItem('dialog'); i++){
                 var ul = document.getElementById("ul_messenger_id");
                 ul.innerHTML += `<li id="ul` + dialogs[i].id + `">
                 <div class="one_message">
@@ -130,5 +141,32 @@ function messenger () {
             </li>
             <li><div class="delimiter"><span>-------------------------------------------------</span></div></li>`;
             }
+            }
+            else{
+                if(parseInt(localStorage.getItem('dialog')) - dialogs.length < 5) {
+                for (let i = localStorage.getItem('dialog') - 5; i < dialogs.length; i++){
+                var ul = document.getElementById("ul_messenger_id");
+                ul.innerHTML += `<li id="ul` + dialogs[i].id + `">
+                <div class="one_message">
+                    <img src="data:image/png;base64,` + dialogs[i].avatar + `"` + `alt="avatar1" class="avatar_messenger">
+                    <a href="https://vk.com/al_im.php" class="block">
+                        <div class="fio">
+                            <span>` + dialogs[i].title + `</span>
+                        </div>
+                    </a>
+                    <div class="last_message">
+                        <span>Пока</span>
+                    </div>
+                    <div class="last_message_time">
+                        <span>18:35</span>
+                    </div>
+                </div>
+            </li>
+            <li><div class="delimiter"><span>-------------------------------------------------</span></div></li>`;
+            }
+            }
+            }
+            $('#mmm').remove()
         });
+
     }
